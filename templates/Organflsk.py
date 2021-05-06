dbf7a10e45088a860837391e0ab4111bd1417383
Maxflo28 <Maxflo13@gmail.com>
2021-05-06 16:20:45 -0700

    updated html narrative

--- templates/Organflsk.py
+++ templates/Organflsk.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-#import pdb; pdb.set_trace()
 @app.route("/")
 def home():
     return render_template("index1.html")
 
 if __name__ == '__main__':
     app.run()
+
+#import datetime
+
+#x = datetime.datetime.now()
+#print(x)
