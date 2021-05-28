c8e24c11d513686c2dc54ea501700fa63146440a
Guillermo Martinez <gmartinez948@outlook.com>
2021-05-28 12:13:26 -0400

    Routed all of the new html paths to the flask app

--- templates/Organflsk.py
+++ templates/Organflsk.py
@@ -7,16 +7,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("index1.html")
+    return render_template("index.html")
+
+@app.route("/Users/guillermo/organproject/OrganProject/Guillermo-HTML/About_us.html")
+def about_us():
+    return render_template("About_us.html")
+
+
+@app.route("/Users/guillermo/organproject/OrganProject/Guillermo-HTML/Resources.html")
+def resources():
+    return render_template("Resources.html")
+
+@app.route("/Users/guillermo/organproject/OrganProject/templates/templates/Research.html")
+def research():
+    return render_template("Research.html")
 
 @app.route("/Users/guillermo/organproject/OrganProject/Guillermo-HTML/Testimonies.html")
 def testimonies():
-    return render_template(Testimonies.html)
+    return render_template("Testimonies.html")
+
+
 # I need other routes to return html for other pages
 # Or create 1 other route and match the path of the route to one of the files
 # ^^ Make a variable & return an html page based on the vriable
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
 
 
